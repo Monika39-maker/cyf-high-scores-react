bf271acd9912a6a91d97852c993c247dc141f12b
Monika39-maker <dangolmonika@yahoo.com>
2022-03-28 22:36:28 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
-
+import PlayerScore from './playerScore';
+import allCountryScores from './scores';
 function App() {
+  // console.log(allCountryScores)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+        <h1>High Scores Per country</h1>
+        <PlayerScore scoreData={allCountryScores}/>
+        
     </div>
   );
 }
