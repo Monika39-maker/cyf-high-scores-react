7e0c3a37f137b3f1c1096f875afe22a36b90561b
Monika39-maker <dangolmonika@yahoo.com>
2022-03-29 13:04:14 +0100

    trying to add all files as some files were not added in prev commit

--- src/App.js
+++ src/App.js
@@ -3,11 +3,11 @@ import './App.css';
 import PlayerScore from './playerScore';
 import allCountryScores from './scores';
 function App() {
-  // console.log(allCountryScores)
+  
   return (
     <div className="App">
       
-        <h1>High Scores Per country</h1>
+        <h1>High Scores Per country:</h1>
         <PlayerScore scoreData={allCountryScores}/>
         
     </div>
---
+++ src/playerScore.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { Table } from 'react-bootstrap';
+import './App.css';
+
+
+
+function PlayerScore({scoreData}) {
+  console.log(scoreData)
+    return (
+      <>
+        
+          {scoreData.map(country => {
+            return (
+              <Table key={country.id}>
+              <thead>
+                <tr className='table-heading'><th>HIGH SCORES: {country.name}</th></tr>
+                <tr>
+                  <th className='bordered'>Player Name</th>
+                  <th className='bordered'>Player Score</th>
+                </tr>
+              </thead> 
+              <tbody>{country.scores.map(player => {
+                return (
+                  <tr className='player-score'>
+                    <td className='bordered'>{player.n}</td>
+                    <td className='bordered'>{player.s}</td>
+                  </tr>
+                )
+              })}
+                
+                
+              </tbody>
+              
+              </Table>
+            )
+          })}
+        
+      </>
+  )
+}
+
+export default PlayerScore
