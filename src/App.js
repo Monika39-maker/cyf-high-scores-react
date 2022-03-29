385cd1536aa27b514a588c57554d923194bd925e
Monika39-maker <dangolmonika@yahoo.com>
2022-03-29 13:24:50 +0100

    sorted by country names, alphabetically

--- src/App.js
+++ src/App.js
@@ -3,7 +3,17 @@ import './App.css';
 import PlayerScore from './playerScore';
 import allCountryScores from './scores';
 function App() {
-  
+  // const sortedAlphabetically = 
+  allCountryScores.sort((countryA, countryB) => {
+          const {name: nameA } = countryA;
+          const {name: nameB } = countryB;
+
+          if (nameA.toLowerCase() < nameB.toLowerCase()) {
+            return -1
+          } else {
+            return 0;
+          }
+        })
   return (
     <div className="App">
       
--- src/playerScore.js
+++ src/playerScore.js
@@ -5,7 +5,7 @@ import './App.css';
 
 
 function PlayerScore({scoreData}) {
-  console.log(scoreData)
+  
     return (
       <>
         
